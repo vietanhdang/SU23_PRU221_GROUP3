73101dd5148e8e6a2787f84ffc79ae34f944c657
Viet Anh Dang <40699842+vietanhdang@users.noreply.github.com>
2023-05-27 23:51:20 +0700

    init solution

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+//Generic Singleton Class
+//Singletons are classes that can only have 1 object (or instance) at a time.
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour {
+
+    private static T instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<T>();
+                if (instance == null)
+                {
+                    GameObject singletonObject = new GameObject(typeof(T).Name);
+                    instance = singletonObject.AddComponent<T>();
+                }
+            }
+
+            return instance;
+        }
+    }
+}
