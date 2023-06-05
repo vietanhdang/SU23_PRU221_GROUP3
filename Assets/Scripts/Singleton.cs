617fedc2fca60445f1d8067a74be7eb27370f0c8
Viet Anh Dang <40699842+vietanhdang@users.noreply.github.com>
2023-06-05 23:45:00 +0700

    Refactor code and fix something

--- Assets/Scripts/Singleton.cs
+++ Assets/Scripts/Singleton.cs
@@ -1,26 +1,26 @@
 ﻿using UnityEngine;
+/// <summary>
+/// Generic Singleton Class
+/// Singletons là các lớp chỉ có thể có 1 đối tượng (hoặc thể hiện) tại một thời điểm.
+/// </summary>
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
 
-//Generic Singleton Class
-//Singletons are classes that can only have 1 object (or instance) at a time.
-public class Singleton<T> : MonoBehaviour where T : MonoBehaviour {
+    private static T instance; // Biến lưu trữ thể hiện duy nhất của lớp
 
-    private static T instance;
-
-    public static T Instance
+    public static T Instance // Thuộc tính lấy thể hiện duy nhất của lớp
     {
         get
         {
             if (instance == null)
             {
-                instance = FindObjectOfType<T>();
-                //Check null
-                if (instance == null)
+                instance = FindObjectOfType<T>(); // Tìm kiếm thể hiện duy nhất của lớp
+                if (instance == null) // Nếu không tìm thấy thì tạo mới
                 {
                     GameObject singletonObject = new GameObject(typeof(T).Name);
                     instance = singletonObject.AddComponent<T>();
                 }
             }
-
             return instance;
         }
     }
